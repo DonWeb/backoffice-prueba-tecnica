7598d3f0ef53b9a72de5ce8d5a293c376243f5dc
Gustavo Campana <gustavo@sistemasdg.com>
2023-03-20 12:47:23 -0300

    2. Servicio listar productos

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('v1/categories', App\Http\Controllers\Api\V1\CategoryController::class)->only('index');
+
+Route::apiResource('v1/products', App\Http\Controllers\Api\V1\ProductController::class)
+    ->only('index');
